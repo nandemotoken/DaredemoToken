a0a74bfa43c7ef4b3ae4f888a05b5ed6bd2e1b40
nandemotoken <nandemotoken@gmail.com>
2020-05-03 10:19:18 +0000

    web3js1.2.7

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ const check = async() => {
 }
 
 const nav_hidari = () => {
-    document.getElementById('nav').style = "position: absolute; bottom: 0px; right: 0px; width: 20%; transform: scale(-1, 1);"
+    document.getElementById('navimg').style = "position: absolute; bottom: 0px; right: 0px; width: 20%; transform: scale(-1, 1);"
 }
 
 const nav_migi = () => {
-    document.getElementById('nav').style = "position: absolute; bottom: 0px; right: 0px; width: 20%; transform: scale(1, 1);"
+    document.getElementById('navimg').style = "position: absolute; bottom: 0px; right: 0px; width: 20%; transform: scale(1, 1);"
 }
 
 const nav_waiting = () => {
