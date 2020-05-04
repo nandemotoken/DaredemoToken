19e3879f8cc60eb5a5817d85e921bf0671e30407
nandemotoken <nandemotoken@gmail.com>
2020-05-04 06:56:05 +0000

    add bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,6 +28,8 @@ const start = async () => {
 }
 
 const displayTokenList = async () => {
+    const d = await $.getJSON("https://api.etherscan.io/api?module=account&action=tokentx&address=0x5B1B0A56461bCA7895D820e36d03a3F1ACde44Bf&startblock=0&endblock=999999999&sort=asc&apikey=9RKFJU66918PAHA44HS5W3PJGPBQCMA3P3")
+    console.log(d)
     t()
     t()
     t()
