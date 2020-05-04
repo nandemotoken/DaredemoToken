a44d79e95e9122d2b1d17b2c52accef71cb42ce9
nandemotoken <nandemotoken@gmail.com>
2020-05-04 07:03:15 +0000

    add bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,8 @@ let provider
 let nav_wait_on = false
 let myaddress
 
+
+
 window.onload = async ()=>{
     authereum = new Authereum('mainnet')
     provider = authereum.getProvider()
@@ -28,7 +30,7 @@ const start = async () => {
 }
 
 const displayTokenList = async () => {
-    const d = await $.getJSON("https://api.etherscan.io/api?module=account&action=tokentx&address=0x5B1B0A56461bCA7895D820e36d03a3F1ACde44Bf&startblock=0&endblock=999999999&sort=asc&apikey=9RKFJU66918PAHA44HS5W3PJGPBQCMA3P3")
+    const d = await $.getJSON(`https://api.etherscan.io/api?module=account&action=tokentx&address=${myaddress}&startblock=0&endblock=999999999&sort=asc&apikey=9RKFJU66918PAHA44HS5W3PJGPBQCMA3P3`)
     console.log(d)
     t()
     t()
