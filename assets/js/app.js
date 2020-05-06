2ddf731bd85dbfc1f10066ec436bca9ac557e49b
nandemotoken <nandemotoken@gmail.com>
2020-05-06 03:29:01 +0000

    maketoken function

--- assets/js/app.js
+++ assets/js/app.js
@@ -177,5 +177,5 @@ const ensaddress = () => {
     addarea.select()
     document.execCommand("copy")
     document.getElementsByTagName("body")[0].removeChild(addarea)
-    window.alert("クリップボードにアドレスをコピーしました\n\n※イーサリアムアドレスはメールアドレスのように使えます")
+    window.alert("クリップボードにアドレスをコピーしました\n\n※メールアドレスのように使えます")
 }
